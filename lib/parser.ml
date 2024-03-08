e8da649cd77fc574f5a5905e48cce5091fbf50ed
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2024-03-08 13:27:18 -0800

    parser: Clean up mess of functions

--- lib/parser.ml
+++ lib/parser.ml
@@ -2,62 +2,30 @@ open! Core
 open! Async
 open! Import
 
-let print_token_err (token : Token.t) message =
-  let where =
-    match token.type_ with
-    | Eof -> " at end"
-    | _ -> sprintf " at '%s'" token.lexeme
+let print_token_err ?token message =
+  let line =
+    match token with
+    | None -> 0
+    | Some token -> token.Token.line
   in
-  print_err ~line:token.line ~where message
+  let where =
+    match token with
+    | None -> ""
+    | Some token ->
+      (match token.type_ with
+       | Eof -> " at end"
+       | _ -> sprintf " at '%s'" token.lexeme)
+  in
+  print_err ~line ~where message
 ;;
 
-module Binary_match = struct
-  type 'a t =
-    | No_match of Token.t list
-    | Match of 'a * Token.t list
-
-  let return tokens = No_match tokens
-
-  let bind t ~f =
-    match t with
-    | Match _ | No_match [] -> t
-    | No_match (token :: tokens) ->
-      (match f token.type_ with
-       | None -> t
-       | Some expr -> Match (expr, tokens))
-  ;;
-
-  let to_option = function
-    | No_match _ -> None
-    | Match (expr, tokens) -> Some (expr, tokens)
-  ;;
-end
-
-let operator ~token_type ~(sym : Expr.operator) (tokens : Token.t list) =
+let match_token (tokens : Token.t list) ~f =
   match tokens with
-  | token :: tokens when Token.Type.equal token.type_ token_type -> Some (sym, tokens)
-  | _ -> None
-;;
-
-let unary ~token_type ~(sym : Expr.expression -> Expr.unary) (tokens : Token.t list) =
-  match tokens with
-  | token :: tokens when Token.Type.equal token.type_ token_type -> Some (sym, tokens)
-  | _ -> None
-;;
-
-let primary ~token_type ~(sym : Expr.literal) (tokens : Token.t list) =
-  match tokens with
-  | token :: tokens when Token.Type.equal token.type_ token_type -> Some (sym, tokens)
-  | _ -> None
-;;
-
-let rec any op_list tokens =
-  match op_list with
   | [] -> None
-  | op :: op_list ->
-    (match op tokens with
-     | Some _ as result -> result
-     | None -> any op_list tokens)
+  | token :: tokens ->
+    (match f token.type_ with
+     | None -> None
+     | Some expr -> Some (expr, tokens))
 ;;
 
 let rec parse_expression tokens = parse_equality tokens
@@ -65,15 +33,12 @@ let rec parse_expression tokens = parse_equality tokens
 and parse_equality tokens =
   let expr, tokens = parse_comparison tokens in
   let rec helper expr tokens =
-    let some_match =
-      Binary_match.return tokens
-      |> Binary_match.bind ~f:(function
+    match
+      match_token tokens ~f:(function
         | Equal -> Some Expr.Equal
         | Bang_equal -> Some Expr.Not_equal
         | _ -> None)
-      |> Binary_match.to_option
-    in
-    match some_match with
+    with
     | None -> expr, tokens
     | Some (op, tokens) ->
       let right, tokens = parse_comparison tokens in
@@ -84,17 +49,14 @@ and parse_equality tokens =
 
 and parse_comparison tokens : Expr.expression * Token.t list =
   let expr, tokens = parse_addition tokens in
-  let some_match =
-    Binary_match.return tokens
-    |> Binary_match.bind ~f:(function
+  match
+    match_token tokens ~f:(function
       | Greater -> Some Expr.Greater
       | Greater_equal -> Some Expr.Greater_equal
       | Less -> Some Expr.Less
       | Less_equal -> Some Expr.Less_equal
       | _ -> None)
-    |> Binary_match.to_option
-  in
-  match some_match with
+  with
   | None -> expr, tokens
   | Some (op, tokens) ->
     let right, tokens = parse_addition tokens in
@@ -103,15 +65,12 @@ and parse_comparison tokens : Expr.expression * Token.t list =
 and parse_addition tokens : Expr.expression * Token.t list =
   let expr, tokens = parse_multiplication tokens in
   let rec helper expr tokens =
-    let some_match =
-      Binary_match.return tokens
-      |> Binary_match.bind ~f:(function
+    match
+      match_token tokens ~f:(function
         | Minus -> Some Expr.Minus
         | Plus -> Some Expr.Plus
         | _ -> None)
-      |> Binary_match.to_option
-    in
-    match some_match with
+    with
     | None -> expr, tokens
     | Some (op, tokens) ->
       let right, tokens = parse_multiplication tokens in
@@ -123,14 +82,12 @@ and parse_addition tokens : Expr.expression * Token.t list =
 and parse_multiplication tokens : Expr.expression * Token.t list =
   let expr, tokens = parse_unary tokens in
   let rec helper expr tokens =
-    let some_match =
-      any
-        [ operator ~token_type:Slash ~sym:Divide
-        ; operator ~token_type:Star ~sym:Multiply
-        ]
-        tokens
-    in
-    match some_match with
+    match
+      match_token tokens ~f:(function
+        | Slash -> Some Expr.Divide
+        | Star -> Some Expr.Multiply
+        | _ -> None)
+    with
     | None -> expr, tokens
     | Some (sym, tokens) ->
       let right, tokens = parse_unary tokens in
@@ -140,14 +97,12 @@ and parse_multiplication tokens : Expr.expression * Token.t list =
   helper expr tokens
 
 and parse_unary tokens : Expr.expression * Token.t list =
-  let some_match =
-    any
-      [ unary ~token_type:Bang ~sym:(fun expr -> Not expr)
-      ; unary ~token_type:Minus ~sym:(fun expr -> Negative expr)
-      ]
-      tokens
-  in
-  match some_match with
+  match
+    match_token tokens ~f:(function
+      | Bang -> Some (fun expr -> Expr.Not expr)
+      | Minus -> Some (fun expr -> Expr.Negative expr)
+      | _ -> None)
+  with
   | None -> parse_primary tokens
   | Some (sym, tokens) ->
     let right, tokens = parse_unary tokens in
@@ -155,31 +110,34 @@ and parse_unary tokens : Expr.expression * Token.t list =
     expr, tokens
 
 and parse_primary tokens =
-  let some_match =
-    any
-      [ primary ~token_type:False ~sym:(Bool false)
-      ; primary ~token_type:True ~sym:(Bool true)
-      ; primary ~token_type:Nil ~sym:Nil (* Number and String *)
-      ]
-      tokens
-  in
-  match some_match with
-  | None ->
-    (match tokens with
-     | token :: tokens ->
-       (match token.type_ with
-        | Identifier s | String s -> Expr.Literal (String s), tokens
-        | Number f -> Expr.Literal (Number f), tokens
-        | Left_paren ->
-          let expr, tokens = parse_expression tokens in
-          (match tokens with
-           | token :: tokens when Token.Type.equal token.type_ Right_paren -> expr, tokens
-           | _ ->
-             print_token_err token "Expect ')' after expression";
-             assert false)
-        | _ ->
-          print_token_err token "Expect expression";
-          assert false)
-     | [] -> assert false)
+  match
+    match_token tokens ~f:(function
+      | False -> Some (Expr.Bool false)
+      | True -> Some (Expr.Bool true)
+      | Nil -> Some Expr.Nil
+      | Identifier s | String s -> Some (Expr.String s)
+      | Number f -> Some (Expr.Number f)
+      | _ -> None)
+  with
   | Some (literal, tokens) -> Expr.Literal literal, tokens
+  | None ->
+    (match
+       match_token tokens ~f:(function
+         | Left_paren -> Some ()
+         | _ -> None)
+     with
+     | None ->
+       print_token_err ?token:(List.hd tokens) "Expect expression";
+       assert false
+     | Some ((), tokens) ->
+       let expr, tokens = parse_expression tokens in
+       (match
+          match_token tokens ~f:(function
+            | Right_paren -> Some ()
+            | _ -> None)
+        with
+        | None ->
+          print_token_err ?token:(List.hd tokens) "Expect ')' after expression";
+          assert false
+        | Some ((), tokens) -> expr, tokens))
 ;;
